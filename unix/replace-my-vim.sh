75094ac68d97d38df7dd9f6f58aee27583a51118
jin.li <jin.li@nufront.com>
2016-08-21 22:55:30 +0800

    Change CMD Fail color

--- unix/replace-my-vim.sh
+++ unix/replace-my-vim.sh
@@ -24,9 +24,10 @@ then
 	git branch -D by_James
 	git branch by_James
 	git checkout by_James
-	git am 0001-powerline-shell-Segments-Add-Segments.patch
-	git am 0002-powerline-shell-Add-segment-git_branch.patch
-	git am 0003-powerline-shell-Fix-no-branch-mistake.patch
+	git am *.patch
+	# git am 0001-powerline-shell-Segments-Add-Segments.patch
+	# git am 0002-powerline-shell-Add-segment-git_branch.patch
+	# git am 0003-powerline-shell-Fix-no-branch-mistake.patch
 
 	#git reset --hard
 	#patch -p1 < 0001-Segments-Add-Segments.patch
