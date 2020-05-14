60385aab01138ded746a46ce880c517fcc1f4e86
李劲 <lijin@consys.com.cn>
2020-05-14 09:47:58 +0800

    fix ubuntu arch detect and ld env setup

--- env_base/common.sh
+++ env_base/common.sh
@@ -72,15 +72,12 @@ elif [[ $SYSTEM_NAME == *"Microsoft"*"Linux"* ]]; then
 	SYSTEM_TYPE="ms-linux"
 elif [[ $SYSTEM_NAME == *"Darwin"* ]]; then
 	SYSTEM_TYPE="mac"
-fi
-
-if [[ $SYSTEM_NAME == *"ubuntu"* || $SYSTEM_NAME == *"kylin"* ]]; then
+elif [[ $SYSTEM_NAME == *"Ubuntu"* || $SYSTEM_NAME == *"kylin"* ]]; then
 	SUB_SYSTEM_TYPE="ubuntu"
 
 	ARCH=$SYSTEM_NAME
 	while true; do
 		GET_ARCH=${ARCH#* }
-		echo $GET_ARCH
 		if [[ $GET_ARCH"x" == $ARCH"x" ]]; then
 			ARCH=$PRE_ARCH
 			break
@@ -310,8 +307,8 @@ fi
 ############# #LD_LIBRARY_PATH Environment ##################
 if [[ "$LD_LIBRARY_PATH" == "" || $LD_LIBRARY_PATH == "/home"* ]]; then
 	SYSTEM_LD_LIBRARY_PATH="/lib:/lib64:/usr/lib:/usr/lib64:/usr/local/lib:/usr/local/lib64"
-	if [[ $SUB_SYSTEM_TYPE"x" == "ubuntu" && ${ARCH}"x" != "x" ]]; then
-		SYSTEM_LD_LIBRARY_PATH="/lib/${ARCH}-gnu-linux:"$SYSTEM_LD_LIBRARY_PATH
+	if [[ $SUB_SYSTEM_TYPE"x" == "ubuntux" && ${ARCH}"x" != "x" ]]; then
+		SYSTEM_LD_LIBRARY_PATH="/lib/${ARCH}-linux-gnu:"$SYSTEM_LD_LIBRARY_PATH
 	fi
 else
 	SYSTEM_LD_LIBRARY_PATH=$LD_LIBRARY_PATH
