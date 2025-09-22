403e34796bc3f94088af8f7e1651baaa5a8d5166
lijin <jinli@syncore.space>
2025-09-22 14:56:13 +0800

    Fix lua config

--- nvim/lua/plugins/vcs.lua
+++ nvim/lua/plugins/vcs.lua
@@ -31,6 +31,35 @@ return {
         "mattn/gist-vim",
     },
     {
-        "APZelos/blamer.nvim"
+        "lewis6991/gitsigns.nvim",
+        config = function()
+            require("gitsigns").setup({
+                signs = {
+                    add = { text = "▎" },
+                    change = { text = "▎" },
+                    delete = { text = "契" },
+                    topdelete = { text = "契" },
+                    changedelete = { text = "▎" },
+                },
+            })
+        end,
+    },
+    {
+        "APZelos/blamer.nvim",
+        dependencies = "nvim-lua/plenary.nvim",
+        event = "BufReadPost",  -- 在缓冲区读取后加载
+        config = function()
+            -- 配置 blamer.nvim
+            vim.g.blamer_enabled = 1  -- 启用插件
+            vim.g.blamer_delay = 1000  -- 设置延迟时间（毫秒）
+            vim.g.blamer_show_in_visual_modes = false  -- 在可视模式中不显示
+            vim.g.blamer_show_inserting = false  -- 在插入模式中不显示
+            vim.g.blamer_prefix = ' > '  -- 设置前缀
+            vim.g.blamer_template = '<author>, <author-time> • <summary>'  -- 设置显示模板
+            vim.g.blamer_date_format = '%Y-%m-%d'  -- 设置日期格式
+            
+            -- 可选：添加切换功能的快捷键
+            vim.keymap.set("n", "<leader>bt", "<cmd>BlamerToggle<cr>", { desc = "Toggle git blame" })
+        end,
     },
 }
