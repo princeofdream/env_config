a63062acfe6a2bd0d9cea7155fd763e6673c9242
lijin <jinli@syncore.space>
2025-09-22 13:48:06 +0800

    Add config for nvim

---
+++ nvim/lua/config/config.lua
@@ -0,0 +1,104 @@
+-- This file will be loaded at the end of init.lua
+-- This file is designed to add your own lua scripts or override the exVim's init.lua settings
+
+-- set list
+-- vim.opt.list = true
+-- vim.opt.listchars = "tab:>-,trail:-"
+
+-- yank data to clipboard
+-- "#" means register.
+-- "+" specifies the system clipboard register.
+-- "y" is yank.
+
+vim.keymap.set("n", "<leader>rt", function()
+    ToggleTabExpand()
+end)
+
+vim.keymap.set("n", "<leader>yy", '"+yy')
+vim.keymap.set("n", "<leader>dd", '"+dd')
+vim.keymap.set("v", "<leader>gg", '"+yy')
+
+vim.opt.expandtab = true
+local toggletabexpand = 1
+
+function ToggleTabExpand()
+    if toggletabexpand == 1 then
+        vim.opt.expandtab = false
+        toggletabexpand = 0
+    else
+        vim.opt.expandtab = true
+        toggletabexpand = 1
+    end
+end
+
+-- Default colorscheme setup
+
+if vim.env.TMUX ~= nil then
+    if vim.opt.termguicolors ~= nil or vim.fn.has("gui_running") == 1 then
+        vim.opt.termguicolors = true
+        vim.g.t_8f = "\x1b[38;2;%lu;%lu;%lum"
+        vim.g.t_8b = "\x1b[48;2;%lu;%lu;%lum"
+    end
+    if vim.fn.has("nvim") == 1 then
+        vim.env.NVIM_TUI_ENABLE_TRUE_COLOR = 1
+    end
+end
+
+local function vim_set_custom_background()
+    if vim.fn.has("gui_running") == 1 then
+        vim.opt.background = "dark"
+    else
+        vim.opt.background = "dark"
+        -- vim.opt.t_Co = 256 -- make sure our terminal use 256 color
+    end
+end
+
+local function vim_set_custom_cursorline()
+    vim.opt.cursorline = true
+    -- vim.api.nvim_set_hl(0, "CursorLine", { cterm = NONE, guibg = "#202727", guifg = NONE })
+    -- vim.opt.cursorcolumn = true
+    -- vim.api.nvim_set_hl(0, "CursorColumn", { cterm = NONE, ctermbg = "darkred", ctermfg = "white", guibg = "darkred", guifg = "white" })
+end
+
+local function vim_set_custom_colorscheme(vim_theme)
+    if vim_theme == "one" then
+        vim.cmd("colorscheme one")
+        -- call one#highlight('vimLineComment', 'cc00cc', '', 'none')
+        -- vim.api.nvim_set_hl(0, "CursorLine", { guibg = "#202727", guifg = NONE })
+        -- vim.api.nvim_set_hl(0, "CursorLine", { guibg = "#FFFFAF", guifg = NONE })
+        -- vim.api.nvim_set_hl(0, "CursorLine", { guibg = "#5c624b", guifg = NONE })
+        -- vim.api.nvim_set_hl(0, "CursorLine", { guibg = "#434837", guifg = NONE })
+        local one_visual_bg = '878777'
+        local one_syntax_fg = {'bcbcbc', '23'}
+        -- local one_syntax_cursor =23', ''}
+        -- one_syntax_bg {'33ff',235'}
+    end
+end
+
+-- Toggle mouse function
+function ToggleMouse()
+    -- check if mouse is enabled
+    if vim.opt.mouse == "a" then
+        -- disable mouse
+        vim.opt.mouse = ""
+    else
+        -- enable mouse everywhere
+        vim.opt.mouse = "a"
+    end
+end
+
+-- keyboard mappings
+vim.keymap.set("n", "<C-g>", function() ToggleMouse() end)
+-- map <C-v> <C-a>
+-- C-x to decrease and C-a/C-s to increase num
+vim.keymap.set("", "<C-s>", "<C-a>")
+-- set clipboard=unnamed
+
+vim.keymap.set("n", "<S-k>", ":tabp<CR>")
+vim.keymap.set("n", "<S-l>", ":tabn<CR>")
+vim.keymap.set("n", "<S-h>", ":bp<CR>")
+vim.keymap.set("n", "<S-j>", ":bn<CR>")
+vim.keymap.set("n", "t[", ":tabp<CR>")
+vim.keymap.set("n", "t]", ":tabn<CR>")
+vim.keymap.set("n", "b[", ":bp<CR>")
+vim.keymap.set("n", "b]", ":bn<CR>")
