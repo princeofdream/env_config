634dd5856f986b70de2ec0a057f34bf07a90e689
lijin <jinli@syncore.space>
2025-09-24 14:48:18 +0800

    disable vim python syntax

--- nvim/lua/plugins/files.lua
+++ nvim/lua/plugins/files.lua
@@ -14,7 +14,7 @@ return {
             vim.keymap.set('n', 'ff', builtin.find_files, { desc = 'Telescope find files' })
             vim.keymap.set('n', 'fg', builtin.live_grep, { desc = 'Telescope live grep' })
             vim.keymap.set('n', 'fb', builtin.buffers, { desc = 'Telescope buffers' })
-            vim.keymap.set('n', 'fh', builtin.help_tags, { desc = 'Telescope help tags' })
+            vim.keymap.set('n', 'fv', builtin.git_branches, { desc = 'Telescope git_branches' })
             require('telescope').setup {
                 defaults = {
                     mappings = {
@@ -73,9 +73,6 @@ return {
     },
     {
         "WolfgangMehner/awk-support",
-    },
-    {
-        "WolfgangMehner/python-support",
     },
     {
         "WolfgangMehner/verilog-support",
