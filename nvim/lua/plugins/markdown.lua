cad7723eeef27b5cadeb39646d5af9b5eb89e930
lijin <jinli@syncore.space>
2025-09-25 12:27:24 +0800

    Change default vv to nvim, fix markdown preview

--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -4,5 +4,16 @@ return {
         cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
         ft = { "markdown" },
         build = function() vim.fn["mkdp#util#install"]() end,
+        config = function()
+            vim.g.mkdp_auto_start = 0
+            vim.g.mkdp_auto_close = 1
+            vim.g.mkdp_refresh_slow = 1
+            vim.g.mkdp_open_to_the_world = 1
+            vim.g.mkdp_echo_preview_url = 1
+            -- vim.g.mkdp_port = ''
+            vim.g.mkdp_browser = "firefox"
+            vim.g.mkdp_page_title = "${name}"
+            vim.g.mkdp_markdown_css = "https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/5.1.0/github-markdown.min.css"
+        end,
     },
 }
