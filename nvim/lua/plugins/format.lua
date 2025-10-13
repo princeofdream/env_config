23052d05b9969905fbaab1f9a50236a354531fc2
lijin <jinli@syncore.space>
2025-10-13 11:48:29 +0800

    Fif cmake-tools depends

--- nvim/lua/plugins/format.lua
+++ nvim/lua/plugins/format.lua
@@ -148,23 +148,6 @@ return {
         end
     },
     {
-        "Civitasv/cmake-tools.nvim",
-        opts = {
-            cmake_executable = "cmake", -- Path to cmake executable
-            cmake_build_directory = "build", -- Path to build directory
-            cmake_generate_options = {}, -- Extra options for cmake generation step
-            cmake_build_options = {}, -- Extra options for cmake build step
-            cmake_console_size = 10, -- Height of the CMake output window
-            cmake_show_console = "always", -- When to show the CMake output window
-            cmake_dap_configuration = { name = "Launch file", type = "codelldb", request = "launch" }, -- DAP configuration for debugging
-            cmake_dap_open_command = function()
-                require("dap").repl.open()
-            end, -- Command to open the DAP repl window
-        },
-        config = function()
-        end
-    },
-    {
         "davidhalter/jedi-vim",
         config = function()
             -- 禁用jedi-vim的文档快捷键
