fd84b878e3639678625fa07664745b9e7a28afa3
lijin <jinli@syncore.space>
2025-10-15 15:16:48 +0800

    fix aiedit

--- nvim/lua/plugins/llm.lua
+++ nvim/lua/plugins/llm.lua
@@ -48,6 +48,40 @@ return {
                     paste_mode = 1,
                 },
             }
+            vim.g.vim_ai_complete = {
+                provider = "openai",
+                prompt = "you are a professional code assistant, help me write code and modify code. \
+                    Do not have any extra explanation, Do Not delete any code, \
+                    just add code based on my requirement, \
+                    Do not use any markdown format in code edit, ",
+                options = {
+                    model= "gpt-4o",
+                    endpoint_url = "http://chat.gxatek.com/api/azure/deployments/gpt-4o/chat/completions",
+                    max_tokens= 0,
+                    max_completion_tokens= 0,
+                    temperature= 0.1,
+                    request_timeout= 20,
+                    stream= 1,
+                    auth_type = "none",
+                    token_file_path= "",
+                    token_load_fn= "",
+                    selection_boundary= "#####",
+                    frequency_penalty= "",
+                    logit_bias= "",
+                    logprobs= "",
+                    presence_penalty= "",
+                    reasoning_effort= "",
+                    seed= "",
+                    stop= "",
+                    top_logprobs= "",
+                    top_p= "",
+                    reasoning= "",
+                },
+                ui= {
+                    paste_mode= 1,
+                },
+            }
+            vim.g.vim_ai_edit = vim.g.vim_ai_complete
         end,
     },
     {
