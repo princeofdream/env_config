57f1914e73558bdcb51b008b73527b2117d922ca
lijin <jinli@syncore.space>
2025-10-16 10:01:05 +0800

    Update color scheme

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -23,7 +23,8 @@ return {
         "princeofdream/gruvcase",
         config = function()
             vim.g.gruvbox_contrast_dark = "soft"
-            vim.cmd([[colorscheme gruvcase]])
+            -- vim.cmd([[colorscheme gruvcase]])
+            vim.cmd([[colorscheme gruvcase_one]])
         end,
     },
     {
