00a9d44030b2b6341d14d0257b4b402d85c21ac5
lijin <jinli@syncore.space>
2025-12-22 17:53:59 +0800

    Add header source switch plugin

--- nvim/lua/plugins/files.lua
+++ nvim/lua/plugins/files.lua
@@ -65,7 +65,32 @@ return {
             vim.keymap.del('i', '<leader>is')
         end,
     },
+    {
+        "jakemason/ouroboros",
+        config = function()
+            require('ouroboros').setup({
+                score_required_to_be_confident_match_is_found = 15,
+                extension_preferences_table = {
+                      -- Higher numbers are a heavier weight and thus preferred.
+                      -- In the following, .c would prefer to open .h before .hpp
+                      c = {h = 11, hpp = 10},
+                      h = {c = 11, cpp = 10},
+                      cpp = {hpp = 11, h = 10},
+                      hpp = {cpp = 11, c = 10},
 
+                      -- Ouroboros supports any combination of filetypes you like, simply
+                      -- add them as desired:
+                      -- myext = { myextsrc = 2, myextoldsrc = 1},
+                      -- tpp = {hpp = 2, h = 1},
+                      -- inl = {cpp = 3, hpp = 2, h = 1},
+                      -- cu = {cuh = 3, hpp = 2, h = 1},
+                      -- cuh = {cu = 1}
+                },
+                switch_to_open_pane_if_possible = true,
+            })
+            vim.g.ouroboros_debug = 0
+        end,
+    },
     {
         "WolfgangMehner/bash-support",
     },
